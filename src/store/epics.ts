657537b3c625f79078768dcb2fceee1f929e3557
JellyLu <634725546@qq.com>
2017-08-11 22:36:39 +0800

    [Yangjie] - combine epics

--- src/store/epics.ts
+++ src/store/epics.ts
@@ -1,7 +1,13 @@
 import { combineEpics } from 'redux-most'
 
 import { epics as userEpic } from '../modules/user/actions'
+import { epics as productEpic } from '../modules/products/actions'
+import { epics as homeEpic } from '../modules/home/actions'
+import { epics as publishProductEpic } from '../modules/publishProduct/actions'
 
 export default combineEpics([
-  ...userEpic,
+    ...userEpic,
+    ...productEpic,
+    ...homeEpic,
+    ...publishProductEpic,
 ])
