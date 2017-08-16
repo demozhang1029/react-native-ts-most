dd1a6b235a17cfffab933926501bfcfc2c01c7e3
JellyLu <634725546@qq.com>
2017-08-16 23:29:13 +0800

    [Yangjie] - 1. Back to loginpopup when click close button on register popup, 2. Go back when click close button on login popup, 3. Show content after user logged in.

--- src/containers/pages/ProfileScreen.tsx
+++ src/containers/pages/ProfileScreen.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react'
+import * as _ from 'lodash'
 import { StyleSheet, View, Dimensions } from 'react-native'
 import { connect, DispatchProp } from 'react-redux'
 import { NavigationActions } from 'react-navigation'
@@ -12,8 +13,7 @@ import { LoginPopup } from '../../components/LoginPopup'
 import { RegisterPopup } from '../../components/RegisterPopup'
 
 export type ProfileProps = DispatchProp<void> & {
-    user: D.User,
-    isUserLogin: boolean,
+    user: D.User
 }
 
 interface ProfileStates {
@@ -25,70 +25,101 @@ class ProfileScreen extends React.Component<ProfileProps, ProfileStates> {
     constructor(props: ProfileProps) {
         super(props)
         this.state = {
-            showLogin: false,
+            showLogin: true,
             showRegister: false
         }
     }
 
     componentDidMount() {
-        const {isUserLogin} = this.props
-        if (!isUserLogin) {
+        this._showOrHideoginPopup(this.props.user.username)
+    }
+
+    componentWillReceiveProps(nextProps: ProfileProps) {
+        this._showOrHideoginPopup(nextProps.user.username)
+    }
+
+    _showOrHideoginPopup = (username: string) => {
+        if (!_.isEmpty(username)) {
             this.setState({
-                showLogin: true
+                showLogin: false,
+                showRegister: false,
+            })
+        } else {
+            this.setState({
+                showLogin: true,
+                showRegister: false,
             })
         }
+    }
+
+    onRegisterClose = () => {
+        this._showOrHideoginPopup(this.props.user.username)
+    }
+
+    onLoginClose = () => {
+        this.props.dispatch(NavigationActions.back())
+    }
+
+    goToRegister = () => {
+        this.setState({
+            showLogin: false,
+            showRegister: true
+        })
+    }
+
+    onLoginUser = (username: string, password: string) => {
+        this.props.dispatch(userLogin({username, password}))
+    }
+
+    _renderContent = () => {
+        return (
+            <View>
+                <Header headerContext="个人信息"/>
+                <View style={styles.container}>
+                    <PersonalInfo name={this.props.user.username}/>
+                    <ButtonWithColor
+                        title="已买宝贝"
+                        onPress={() => {
+                            this.props.dispatch(NavigationActions.navigate({routeName: 'bought-products'}))
+                        }}
+                    />
+                    <ButtonWithColor
+                        title="出售宝贝"
+                        onPress={() => {
+                            this.props.dispatch(NavigationActions.navigate({routeName: 'sold-out'}))
+                        }}
+                    />
+                    <ButtonWithColor
+                        title="退出登陆"
+                        onPress={() => {
+                            this.props.dispatch(userLogout(this.props.user))
+                            this.props.dispatch(NavigationActions.navigate({routeName: 'homePage'}))
+                        }}
+                    />
+                </View>
+            </View>
+        )
     }
 
     render() {
         return (
             <View>
                 {
-                    (!this.state.showLogin && !this.state.showRegister)
-                    &&
-                    <View>
-                        <Header headerContext="个人信息"/>
-                        <View style={styles.container}>
-                            <PersonalInfo name="Yangjie"/>
-                            <ButtonWithColor
-                                title="已买宝贝"
-                                onPress={() => {
-                                    this.props.dispatch(NavigationActions.navigate({routeName: 'bought-products'}))
-                                }}
-                            />
-                            <ButtonWithColor
-                                title="出售宝贝"
-                                onPress={() => {
-                                    this.props.dispatch(NavigationActions.navigate({routeName: 'sold-out'}))
-                                }}
-                            />
-                            <ButtonWithColor
-                                title="退出登陆"
-                                onPress={() => {
-                                    this.props.dispatch(userLogout(this.props.user))
-                                    this.props.dispatch(NavigationActions.navigate({routeName: 'home'}))
-                                }}
-                            />
-                        </View>
-                    </View>
+                    (!this.state.showLogin && !this.state.showRegister) && this._renderContent()
                 }
                 {
                     this.state.showLogin
                         ? <LoginPopup
-                            onSubmit={this.props.dispatch.bind(null, userLogin)}
+                            onSubmit={this.onLoginUser}
                             isActive={true}
-                            goToRegister={
-                                () => this.setState({
-                                    showLogin: false,
-                                    showRegister: true
-                                })
-                            }
-                            onIconClick={() => this.setState({showLogin: false})}
+                            goToRegister={this.goToRegister}
+                            onIconClick={this.onLoginClose}
                         />
                         : this.state.showRegister
                         ? <RegisterPopup
                             onSubmit={this.props.dispatch.bind(null, userRegister)}
                             isActive={true}
-                            onIconClick={() => this.setState({showRegister: false})}
+                            onIconClick={this.onRegisterClose}
                         />
                         : null
                 }
